c4a34d5c8f39d11ca39c84a8d874463f49fbf85b
keita-s-1129 <k1129.sasaki@gmail.com>
2024-11-15 17:47:46 +0900

    ActiveHashの導入、バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,15 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
+  belongs_to :category
+  belongs_to :description
+  belongs_to :condition
+  belongs_to :prefecture
+  belongs_to :delivery_day
+
+  validates :category_id, numericality:      { other_than: 1, message: "can't be blank" }
+  validates :status_id, numericality:        { other_than: 1, message: "can't be blank" }
+  validates :shipping_fee_id, numericality:  { other_than: 1, message: "can't be blank" }
+  validates :prefecture_id, numericality:    { other_than: 1, message: "can't be blank" }
+  validates :delivery_time_id, numericality: { other_than: 1, message: "can't be blank" }
 end
