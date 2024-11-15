8c9c0c8a3d682b9f454eee68a0f8c2bf9ffcae13
keita-s-1129 <k1129.sasaki@gmail.com>
2024-11-15 12:02:48 +0900

    Userモデルの単体テスト

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,9 +12,7 @@ class User < ApplicationRecord
   validates :password, format: { with: PASSWORD_REGEX, message: 'is invalid. Include both letters and numbers' }
   validates :first_name, presence: true, format: { with: NAME_REGEX, message: 'is invalid. Input full-width characters' }
   validates :last_name, presence: true, format: { with: NAME_REGEX, message: 'is invalid. Input full-width characters' }
-  validates :first_kana, presence: true,
-                         format: { with: KANA_REGEX, message: 'is invalid. Input full-width katakana characters' }
-  validates :last_kana, presence: true,
-                        format: { with: KANA_REGEX, message: 'is invalid. Input full-width katakana characters' }
+  validates :first_kana, presence: true, format: { with: KANA_REGEX, message: 'is invalid. Input full-width katakana characters' }
+  validates :last_kana, presence: true, format: { with: KANA_REGEX, message: 'is invalid. Input full-width katakana characters' }
   validates :birthday, presence: true
 end
