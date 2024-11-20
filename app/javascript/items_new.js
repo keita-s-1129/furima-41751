ee7c92f62e55fa80d2e11e8856c8e8b05aacef83
keita-s-1129 <k1129.sasaki@gmail.com>
2024-11-20 12:02:58 +0900

    商品出品機能　修正

--- app/javascript/items_new.js
+++ app/javascript/items_new.js
@@ -1,14 +1,16 @@
-document.addEventListener('DOMContentLoaded', () => {
+function setPriceEventListener() {
   const priceInput = document.getElementById("item-price");
   const addTaxDom = document.getElementById("add-tax-price");
   const profitDom = document.getElementById("profit");
+
+  if (!priceInput) return;
 
   priceInput.addEventListener("input", () => {
     const inputValue = priceInput.value;
 
     if (inputValue >= 300 && inputValue <= 9999999) {
       const tax = Math.floor(inputValue * 0.1);
-      const profit = inputValue - tax;
+      const profit = Math.floor(inputValue - tax);
 
       addTaxDom.innerHTML = tax.toLocaleString();
       profitDom.innerHTML = profit.toLocaleString();
@@ -17,4 +19,8 @@ document.addEventListener('DOMContentLoaded', () => {
       profitDom.innerHTML = '';
     }
   });
-});
+}
+
+document.addEventListener('DOMContentLoaded', setPriceEventListener);
+document.addEventListener('turbo:load', setPriceEventListener);
+document.addEventListener('turbo:render', setPriceEventListener);
